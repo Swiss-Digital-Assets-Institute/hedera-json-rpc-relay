fcfe377b0796a2339ec611fa0daa280321f10193
Tomasz Lagodzinski <gothrek22@users.noreply.github.com>
2023-08-07 14:49:56 +0200

    Fix image name.

--- deploy/set_images.sh
+++ deploy/set_images.sh
@@ -3,5 +3,5 @@
 IFS=',' read -ra SERVICE_LIST <<<$SERVICES
 
 for service in "${SERVICE_LIST[@]}"; do
-  SERVICE=$service yq eval -i '.services.[env(SERVICE)].image="$REGISTRY$SERVICE:$IMAGE_TAG"' docker-compose.yml
+  SERVICE=$service TAG="${REGISTRY}${SERVICE}:${IMAGE_TAG}" yq eval -i '.services.[env(SERVICE)].image = env(TAG)' docker-compose.yml
 done
