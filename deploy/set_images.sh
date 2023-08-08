c14b59c69241c7b98ef3780f3cb0ce21f0f9a2fa
Tomasz Lagodzinski <gothrek22@users.noreply.github.com>
2023-08-08 09:52:55 +0200

    Fix inconsistent image naming.

--- deploy/set_images.sh
+++ deploy/set_images.sh
@@ -3,5 +3,5 @@
 IFS=',' read -ra SERVICE_LIST <<<$SERVICES
 
 for service in "${SERVICE_LIST[@]}"; do
-  SERVICE=$service TAG="${REGISTRY}${SERVICE}:${IMAGE_TAG}" yq eval -i '.services.[env(SERVICE)].image = env(TAG)' docker-compose.yml
+  SERVICE=$service TAG="${REGISTRY}${APP_NAME}-${SERVICE}:${IMAGE_TAG}" yq eval -i '.services.[env(SERVICE)].image = env(TAG)' docker-compose.yml
 done
